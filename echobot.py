2df2898ca7c88b77e7103793d7de59e1471c6c36
dev-yoga <spin_shoppe@icloud.com>
2022-03-19 22:51:47 -0700

    echo bot

---
+++ echobot.py
@@ -0,0 +1,27 @@
+print("Greetings! I am a basic echo bot.")
+
+print("I will repeat what you tell me, like a parrot.")
+
+stuff_to_say_one = input("What would you have me say?")
+
+print(stuff_to_say_one)
+
+stuff_to_say_two = input("And now?")
+
+print(stuff_to_say_two)
+
+stuff_to_say_three = input("Mmmhmm?")
+
+print(stuff_to_say_three)
+
+stuff_to_say_four = input("And so?")
+
+print(stuff_to_say_four)
+
+stuff_to_say_five = input("Is there more?")
+
+print(stuff_to_say_five)
+
+stuff_to_say_six = input("This is the last prompt.")
+
+print(stuff_to_say_six)
