5653ff780a5c48a198b1d1759648ef333a09d990
dev-yoga <spin_shoppe@icloud.com>
2022-03-25 19:47:08 -0700

    oranges total

--- twodlist.py
+++ twodlist.py
@@ -7,3 +7,14 @@ print("You have", produce[1][1], "oranges.")
 oranges_total = produce[1][1]
 
 print(oranges_total)
+
+produce[1][1] = 3
+
+print(oranges_total)
+print(produce[1][1])
+
+#interestingly it doesn't update oranges_total so you have to update oranges total
+
+oranges_total = produce[1][1]
+
+print(oranges_total)
