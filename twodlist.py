4ce1cc14ea27b1842024a7778f6f5ae4b378de96
dev-yoga <spin_shoppe@icloud.com>
2022-03-25 19:41:03 -0700

    oranges

--- twodlist.py
+++ twodlist.py
@@ -3,3 +3,7 @@ produce = [["Mangoes", 3], ["Oranges", 4], ["Pineapples", 2], ["Coconuts", 1]]
 print(produce)
 print(produce[1])
 print("You have", produce[1][1], "oranges.")
+
+oranges_total = produce[1][1]
+
+print(oranges_total)
