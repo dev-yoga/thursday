90b1486ea77d66667770f08c6142b4c65821882a
dev-yoga <spin_shoppe@icloud.com>
2022-03-28 22:31:16 -0700

    count

--- listmethods.py
+++ listmethods.py
@@ -28,3 +28,11 @@ print(more_fruits)
 
 sliced_fruits = more_fruits[2:4]
 print(sliced_fruits)
+
+favorite_fruit_responses = ["orange", "orange", "apple", "orange", "orange", "orange", "mango", "banana", "banana", "orange", "orange", "persimmon"]
+
+orange_count = favorite_fruit_responses.count("orange")
+banana_count = favorite_fruit_responses.count("banana")
+
+print(orange_count)
+print(banana_count)
