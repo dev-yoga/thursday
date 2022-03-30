07b46332aa9fc37309037a4f1461c34dc9984022
dev-yoga <spin_shoppe@icloud.com>
2022-03-29 22:19:51 -0700

    zip

---
+++ zip.py
@@ -0,0 +1,7 @@
+plants = ["Monstera", "Zz", "Snake", "Bird of Paradise", "Fiddle Leaf Fig", "Monstera Adansonii", "Ponytail Palm"]
+
+inventory = [3, 4, 2, 3, 5, 3, 3]
+
+plants_inventory = zip(plants, inventory)
+
+print(list(plants_inventory))
