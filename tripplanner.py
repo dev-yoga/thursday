4217fb530dded212a546721c151aef9686c35521
dev-yoga <spin_shoppe@icloud.com>
2022-04-04 20:54:21 -0700

    Estimated time

--- tripplanner.py
+++ tripplanner.py
@@ -1,3 +1,9 @@
 def trip_planner_welcome(name): print("Welcome to triplplanner v1.0, " + name + "!")
 
 trip_planner_welcome("Your Name")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(2.5)
