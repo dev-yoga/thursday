f1fa1e6cba05bf97a803b86ef4c4f9f73fa3bd9f
dev-yoga <spin_shoppe@icloud.com>
2022-04-09 19:52:13 -0700

    in range

--- loops.py
+++ loops.py
@@ -2,3 +2,10 @@ fruits = ["mango", "orange", "pineapple", "guava", "papaya"]
 
 for fruit in fruits:
     print(fruit)
+
+# It's a fruit loop, get it? :D
+
+# Okay that's probably not funny xD
+
+for fruit in range(4):
+    print(fruits)
