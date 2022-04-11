6b988466a420acc7eb731c4c6ffe06ca65509f08
dev-yoga <spin_shoppe@icloud.com>
2022-04-10 20:27:07 -0700

    berries and while loops

---
+++ whileloop.py
@@ -0,0 +1,8 @@
+fruits = ["strawberries", "blueberries", "raspberries"]
+
+length = len(fruits)
+index = 0
+
+while index < length:
+    print("I should pick some " + fruits[index] + ".")
+    index += 1
