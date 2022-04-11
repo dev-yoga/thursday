7dc5fb2e6211cd35737b1288ddcbd3a3f45fdfb0
dev-yoga <spin_shoppe@icloud.com>
2022-04-10 19:43:50 -0700

    creating a class

---
+++ puppyclass.py
@@ -0,0 +1,8 @@
+class Puppy:
+  def __init__(self, input_name, input_breed, input_age = 0, input_friendliness = True):
+
+      self.name = input_name
+      self.breed = input_breed
+      self.age = input_age
+      self.is_friendly = input_friendliness
+
