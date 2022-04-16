e9b3714a1067951c41ce58fe4d44b122de917add
dev-yoga <spin_shoppe@icloud.com>
2022-04-15 21:22:58 -0700

    lists and loops

---
+++ producesection.py
@@ -0,0 +1,8 @@
+produce = ["cucumbers", "berries", "tomatoes", "kale", "carrots", "avocados", "oranges", "mangoes", "pineapples", "cilantro"]
+
+produce_I_want = "mangoes"
+
+for fruits_and_veggies in produce:
+    print(fruits_and_veggies)
+
+
