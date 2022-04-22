f03507b8ad874a78146188c6f54aa00de693cb8c
dev-yoga <spin_shoppe@icloud.com>
2022-04-21 22:53:06 -0700

    squares, cubes

---
+++ squarescubes.py
@@ -0,0 +1,14 @@
+single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+squares = []
+
+for num in single_digits:
+  print(num)
+  squares.append(num**2)
+  print(squares)
+
+cubes = []
+for num in single_digits:
+  print(num)
+  cubes.append(num**3)
+  print(cubes)
