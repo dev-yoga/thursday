fb56fd0d8e71d2987fd479725cbe702580d68934
dev-yoga <spin_shoppe@icloud.com>
2022-04-21 22:45:27 -0700

    enough mangoes

--- listcomprehensions.py
+++ listcomprehensions.py
@@ -2,3 +2,6 @@ mango_batches = [2, 4, 6, 8, 10, 12, 14, 16]
 mangoes_added = [num + 2 for num in mango_batches]
 
 print(mangoes_added)
+
+enough_mangoes = [mango for mango in mango_batches if mango > 10]
+print(enough_mangoes)
