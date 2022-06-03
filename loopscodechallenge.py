7adcbe4957e3f8582f7be5b9107b72e2259a0022
dev-yoga <spin_shoppe@icloud.com>
2022-06-02 21:22:19 -0700

    loops challenge 1

---
+++ loopscodechallenge.py
@@ -0,0 +1,8 @@
+def divisible_by_ten(nums):
+    count = 0
+    for number in nums:
+        if(number % 10 == 0):
+            count += 1
+    return count
+
+print(divisible_by_ten([20, 25, 30, 35, 40]))
